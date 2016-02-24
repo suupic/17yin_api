445708c009ddb006feeb1b3bc7ef40f0fb593aa4
xuyannan <xyn0563@gmail.com>
2016-02-24 12:52:15 +0800

    support extra param from order processing

--- test/test.js
+++ test/test.js
@@ -1,22 +1,23 @@
 var Api = require('../lib/api.js');
 var api = new Api('http://localhost:3000/api/v1/');
 var Base64 = require('base-64');
-api.login('18618127785', 'nanzi17yin').then(
-  function(res){},
-  function(res){console.log(res);}
-);
-
 var token = Base64.encode('18600000001:yin17173');
-api.getTasks(token).then(
-  function(tasks) {
-    console.log(tasks);
-  },
-  function(error) {
-    console.log(error);
-  }
-)
+// api.login('18618127785', 'nanzi17yin').then(
+//   function(res){},
+//   function(res){console.log(res);}
+// );
+//
 
-api.processOrder('151114064180', 'ship', token).then(
+// api.getTasks(token).then(
+//   function(tasks) {
+//     console.log(tasks);
+//   },
+//   function(error) {
+//     console.log(error);
+//   }
+// )
+
+api.processOrder('151114085371', 'finish', token, 'test memo').then(
   function(order) {
     console.log(order);
   },
