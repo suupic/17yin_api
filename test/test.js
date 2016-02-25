d410dd35883602f45e51f8813d08cc5963ec9342
xuyannan <xyn0563@gmail.com>
2016-02-25 11:58:14 +0800

    错误处理

--- test/test.js
+++ test/test.js
@@ -3,10 +3,11 @@ var api = new Api('http://localhost:3000/api/v1/');
 var Base64 = require('base-64');
 var token = Base64.encode('18600000001:yin17173');
 // api.login('18618127785', 'nanzi17yin').then(
-//   function(res){},
+//   function(res){
+//     console.log('login success');
+//   },
 //   function(res){console.log(res);}
 // );
-//
 
 // api.getTasks(token).then(
 //   function(tasks) {
@@ -16,7 +17,7 @@ var token = Base64.encode('18600000001:yin17173');
 //     console.log(error);
 //   }
 // )
-
+//
 api.processOrder('151114085371', 'finish', token, 'test memo').then(
   function(order) {
     console.log(order);
